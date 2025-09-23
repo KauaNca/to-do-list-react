6ddbb21e706c8a0df1a195a41b8a98e6b4f81ac5
KauaNca <git@stackblitz.com>
2025-09-23 10:21:08 -0300

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+//import { useState } from 'react';
+import './App.css';
+import ButtonUsage from './components/layout/Button.jsx';
+
+function App() {
+  return (
+    <div>
+      <ButtonUsage />
+    </div>
+  );
+}
+
+export default App;
