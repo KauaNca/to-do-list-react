621b4614581841753134c011b02ea33a4a0192db
Kauã Nunes <kauanunes2003@gmail.com>
2025-09-25 10:50:01 -0300

    arrumando

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,13 @@
 //import { useState } from 'react';
 import './App.css';
-import Input from './components/form/Input';
+import Input from './components/form/Input.jsx';
+import ButtonForm from './components/layout/Button.jsx';
 
 function App() {
   return (
     <>
     <Input name='field-search' label='Pesquisar'/>
-    <Button variant='contained' texto='Asc' handleOnChange={(e)=>{}}/>
+    <ButtonForm variant='contained' texto='Asc' handleOnChange={()=>{}}/>
     </>
 
   );
--- src/components/form/Input.jsx
+++ src/components/form/Input.jsx
@@ -2,7 +2,7 @@ import * as React from 'react';
 import Box from '@mui/material/Box';
 import TextField from '@mui/material/TextField';
 
-function Input({ id, name, label }) {
+function Input({ name, label }) {
     return (
         <>
             <TextField
