7ea46311e4950dbc36861b88f1d4b613049dbe66
Kauã Nunes <kauanunes2003@gmail.com>
2025-09-30 10:41:16 -0300

    kaua - adicionando tarefas

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,49 @@
 import "./App.css";
 import SimpleContainer from "./components/layout/Container";
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import Input from "./components/form/Input";
 import Botao from "./components/form/Botao";
 import { CheckCircle2 } from "lucide-react";
+import AssignmentOutlinedIcon from "@mui/icons-material/AssignmentOutlined";
 
 function App() {
   const handleAddTask = () => {
-    console.log("Adicionar tarefa");
+    const novaTarefa = { titulo: input, concluida: false };
+    fetch("http://localhost:5000/tarefas", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(novaTarefa),
+    })
+      .then((resp) => resp.json())
+      .then((data) => {
+        console.log("Tarefa adicionada:", data);
+      })
+      .catch((error) => {
+        console.error("Erro ao adicionar tarefa:", error);
+      });
   };
+  function buscarTarefas() {
+    fetch("http://localhost:5000/tarefas", {
+      method: "GET",
+      headers: {
+        "Content-Type": "application/json",
+      },
+    })
+      .then((response) => response.json())
+      .then((data) => {
+        setTarefa(data);
+        console.log(data);
+      })
+      .catch((error) => {
+        console.error("Erro ao buscar tarefas:", error);
+      });
+  }
+  useEffect(buscarTarefas, []); //Quando o componente for montado, buscar as tarefas
+
   const [tarefa, setTarefa] = useState([]); // Estado para armazenar as tarefas
-
-
+  const [input, setInput] = useState("");
   return (
     <>
       <SimpleContainer
@@ -34,15 +66,37 @@ function App() {
           </p>
         </div>
         <div className="input-container">
-          <Input placeholder="Digite sua nova tarefa..." />
+          <Input
+            type="text"
+            placeholder="Digite sua nova tarefa..."
+            value={input}
+            onChange={(e) => setInput(e.target.value)}
+          />
           <Botao onClick={handleAddTask} />
         </div>
         {/* Renderize a lista de tarefas aqui */}
         {tarefa.length === 0 && (
-          <p className="text-center text-muted-foreground mt-4">
-            Nenhuma tarefa adicionada.
-          </p>
+          <SimpleContainer
+            sxContainer={{
+              backgroundColor: "white",
+              textAlign: "center",
+              marginTop: "20px",
+              padding: "20px",
+              borderRadius: "8px",
+              boxShadow: "0 2px 4px rgba(0, 0, 0, 0.1)",
+            }}
+          >
+            <AssignmentOutlinedIcon style={{ fontSize: 70, color: "black" }} />
+            <p style={{ fontSize: 20, color: "black", fontWeight: "bold" }}>
+              Nenhuma tarefa ainda
+            </p>
+            <p style={{ fontSize: 16, color: "gray" }}>
+              Comece adicionando sua primeira tarefa acima!{" "}
+            </p>
+          </SimpleContainer>
         )}
+
+        {tarefa.length > 0 && <p>Há tarefas</p>}
       </SimpleContainer>
     </>
   );
